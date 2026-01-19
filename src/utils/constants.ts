9a107b7931a5f405eca91c612005fe18af72b509
BioTomateDE <latus.kati@gmail.com>
2026-01-19 08:31:21 +0100

    Improvedingon that plugin. now doesnt fuckington up hyprlinks (#658)
    * Improvedingon that plugin. now doesnt fuckington up hyprlinks
    
    * bitchass ==
    
    * bleh
    
    ---------
    
    Co-authored-by: thororen <78185467+thororen1234@users.noreply.github.com>

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1241,6 +1241,10 @@ export const EquicordDevs = Object.freeze({
         name: "busyboxkitty",
         id: 1312185484159483948n
     },
+    BioTomateDE: {
+        name: "BioTomateDE",
+        id: 553499669226061844n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
