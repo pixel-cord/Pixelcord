70b1c2d112f83cb25d153b83e024aa998badc9b0
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-02-06 21:17:11 -0500

    Questify: Fix Patches

--- src/equicordplugins/questify/index.tsx
+++ src/equicordplugins/questify/index.tsx
@@ -1498,7 +1498,7 @@ export default definePlugin({
                     // Run Questify's sort function every time due to hook requirements but return
                     // early if not applicable. If the sort method is set to "Questify", replace the
                     // Quests with the sorted ones. Also, setup a trigger to rerender the memo.
-                    match: /(?<=function \i\((\i),\i\){let \i=\i.useRef.{0,100}?;)(return \i.useMemo\(\(\)=>{)/,
+                    match: /(?<=function \i\((\i),\i\){let \i=.{0,10}\i.useRef.{0,100}?;)(return \i.useMemo\(\(\)=>{)/,
                     replace: "const questRerenderTrigger=$self.useQuestRerender();const questifySorted=$self.sortQuests($1,arguments[1].sortMethod!==\"questify\");$2if(arguments[1].sortMethod===\"questify\"){$1=questifySorted;};"
                 },
                 {
@@ -1508,7 +1508,7 @@ export default definePlugin({
                 },
                 {
                     // If we already applied Questify's sort, skip further sorting.
-                    match: /(?<=sortMethod:(\i).{0,100}?\)\);)(return )((\i).sort)/,
+                    match: /(?<=sortMethod:(\i).{0,100}?\i\);)(return )((\i).sort)/,
                     replace: "$2$1===\"questify\"?$4:$3"
                 },
                 {
