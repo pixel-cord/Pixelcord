7d4368188ae2b2d535a60dcff834616ffaab853a
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-02-17 14:47:21 -0500

    UserAreaAPI: Fix Conflict

--- src/equicordplugins/_api/userArea.ts
+++ src/equicordplugins/_api/userArea.ts
@@ -27,7 +27,7 @@ export default definePlugin({
                 },
                 // fix discord weird shrink with extra buttons
                 {
-                    match: /(?<=\{ref:\i,)style:(\i)(?=,onMouseEnter:\i,onMouseDown:\i,onClick)/,
+                    match: /(?<=\{ref:\i,)style:(\i)/,
                     replace: "style:{...$1,minWidth:0}"
                 }
             ]
