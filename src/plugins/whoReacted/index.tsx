435eb9efbf883feee4bcc35304216f13c2bfc362
creations <creations@creations.works>
2026-05-05 13:13:11 -0400

    fix whoreacted (#1051)
    * fix whoreacted
    
    * make it a bit smaller
    
    * make it better lol

--- src/plugins/whoReacted/index.tsx
+++ src/plugins/whoReacted/index.tsx
@@ -96,8 +96,8 @@ export default definePlugin({
         {
             find: ",reactionRef:",
             replacement: {
-                match: /(\i)\?null:\(0,\i\.jsx\)\(\i\.\i,{className:\i\.reactionCount,.*?}\),/,
-                replace: "$&$1?null:$self.renderUsers(this.props),"
+                match: /\(0,\i\.jsx\)\(\i\.\i,\{className:\i\.reactionCount,.{0,60}\}\),/,
+                replace: "$&$self.renderUsers(arguments[0]),"
             }
         },
         {
@@ -122,6 +122,7 @@ export default definePlugin({
     },
 
     renderUsers(props: RootObject) {
+        if (!props?.message || props.hideCount) return null;
         return props.message.reactions.length > 10 ? null : (
             <ErrorBoundary noop>
                 <this.UsersComponent {...props} />
@@ -197,6 +198,7 @@ interface RootObject {
     burst_me: boolean;
     me: boolean;
     type: number;
+    hideCount: boolean;
     hideEmoji: boolean;
     remainingBurstCurrency: number;
 }
